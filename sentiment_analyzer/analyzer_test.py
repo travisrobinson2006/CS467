5e70f2b3278891dc08cd8e525f2b197d222a6be0
Travis Robinson <travisrobinson2006@gmail.com>
2017-02-10 09:06:43 -0800

    Upload of corrected test file

--- sentiment_analyzer/analyzer_test.py
+++ sentiment_analyzer/analyzer_test.py
@@ -1,10 +1,33 @@
 import sentiment_analyzer_working_title as analyzer
 
 TEST_TERMS = [
-"breaking bad 	Breaking Bad. I'm at the very end of the series and its basically just one big shit fest 	OH",
-"law and order svu 	I love law and order svu, never gets old ... 	LA",
+"the bachelor	Finally watched yesterday's episode of the bachelor and so happy with who went home 	CA",
+"the bachelor	Trump's treating it more like \"The Bachelor\". I expect him to hand out a rose 	TX]"
 ]
 
-for i in TEST_TERMS:
+with open("sample_tweets.txt") as tweets:
+	tweets = tweets.readlines()
+	for i in tweets:
+		print(analyzer.tweet_analyzer(i))
+
+tweetfile = open("sample_tweets.txt", "r")
+
+tweetlist = []
+for line in tweetfile:
+	tweetlist.append(line)
+
+for i in tweetlist:
+#	print i
 	print(analyzer.tweet_analyzer(i))
+
+#for i in TEST_TERMS:
+#	print(analyzer.tweet_analyzer(i))
+#	analyzer.tweet_analyzer(i)
+
+
+
+#	print(tweets)
+
+#for i in TEST_TERMS:
+#	print(analyzer.tweet_analyzer(i))
 #	analyzer.tweet_analyzer(i)
