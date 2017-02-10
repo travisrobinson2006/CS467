e14897734bacbcf2556afd8270d180edd99527e6
Travis Robinson <travisrobinson2006@gmail.com>
2017-02-09 16:36:57 -0800

    Tweet Text Optimizer Written
    Will remove undesired words as per the list

--- sentiment_analyzer/sentiment_analyzer_working_title.py
+++ sentiment_analyzer/sentiment_analyzer_working_title.py
@@ -1,4 +1,3 @@
-import sentiment_analyzer_lib.settings as settings
 import sentiment_analyzer_lib.sentiment_analyzer_config as  config
 import csv
 import re
@@ -7,14 +6,18 @@ import re
 def optimize_tweet_text(tweet_text):
 	tweet_text = tweet_text.lower()
 	#remove pronouns, prepositions, conjunctions, etc
-	return tweet_text
-
-#retrive show name
-def get_show_name(tweet_text):
-	for i in settings.TRACK_TERMS:
-		if i.lower() in tweet_text:
-			return i.lower()
-	return False
+	tweet_text_return = ""
+	for i in tweet_text.split():
+		word_needed = True
+		for x in config.UNNEEDED_WORDS:
+			if re.search(r"\b" + re.escape(i.lower()) + r"\b", x.lower()):
+				word_needed = False
+				break
+		if word_needed == True:
+			tweet_text_return = tweet_text_return + i + " "
+#		if i.lower() not in config.UNNEEDED_WORDS:
+#			tweet_text_temp = tweet_text_temp + i + " "
+	return tweet_text_return.strip()
 
 #get show sentiment, return a number
 def get_show_sentiment(tweet_text):
@@ -23,15 +26,15 @@ def get_show_sentiment(tweet_text):
 #analyze tweet, return state, show name, and sentiment score
 def tweet_analyzer(tweet_data):
 	try:#error control in case we get invalid states, we want to return false
+		show_name = tweet_data.split("\t")[config.TWEET_NAME_COLUMN].strip()
 		tweet_text = tweet_data.split("\t")[config.TWEET_TEXT_COLUMN].strip()
 		tweet_state = tweet_data.split("\t")[config.TWEET_STATE_COLUMN].strip()
-		if tweet_state == "" or tweet_text == "":
+		if tweet_state == "" or tweet_text == "" or show_name == "":
 			return False
 	except IndexError:
 		return False
 
 	tweet_text = optimize_tweet_text(tweet_text)#could do one line, want it readable
-	show_name = get_show_name(tweet_text)#get show name; may not be efficient memory-wise to do this way but makes easier to read
 
 	if show_name == False or tweet_state == False:
 		return False
@@ -42,13 +45,11 @@ def tweet_analyzer(tweet_data):
 			usable_row = row[0].split(",")#convert row into a list so we can check if tweet word is in there, get the sentiment
 			if re.search(r"\b" + re.escape(usable_row[1]) + r"\b", tweet_text):#use regex since we need whole words-http://stackoverflow.com/questions/4154961/find-substring-in-string-but-only-if-whole-words
 				print(usable_row[1] + ": " + usable_row[2])
-
-
 	show_sentiment = get_show_sentiment(tweet_text)
 #	if show_sentiment == 0:
 		#send show text to another file, so we can examine for possible addition to dictionary
 
 	#put sentiment, show name and state into results
-
+	return [show_name,tweet_state,show_sentiment]
 
 
